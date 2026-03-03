f16441111fb2ecee8d44dd97a1d885238f07f7a7
sadaqsam <sadaqsamor99@gmail.com>
2026-03-03 14:27:18 +0300

    SomeeDeynamicNavLink

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,28 +1,44 @@
 import React from 'react'
 import { Contact2Icon, HomeIcon, IdCard, User2 } from 'lucide-react'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
     const navLinks = [
         {
             name:"Home",
-            icon: <HomeIcon/>
+            icon: <HomeIcon/>,
+            path: "/"
         } ,
         {
             name:"About",
-            icon: <User2/>
+            icon: <User2/>,
+            path: "/"
         }   ,
         {
             name:"Resume",
-            icon: <IdCard/>
+            icon: <IdCard/>,
+            path: "/"
         }   ,
         {
             name:"Contact-me",
-            icon: <Contact2Icon/>
+            icon: <Contact2Icon/>,
+            path: "/"
         }   
     ]
   return (
-    <div className=''>
-
+    <div className='flex justify-between font-bold px-3 py-2 bg-gray-700'>
+        <h1>samo</h1>
+        <div className='flex space-x-4'>
+            {
+                navLinks.map((link)=>(
+                    <NavLink to={link.path} className='flex gap-4'>
+                        <span>{link.icon}</span>
+                        {link.name}
+                    </NavLink>
+                ))
+            }
+        </div>
+        
     </div>
   )
 }
